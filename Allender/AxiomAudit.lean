6ea5bb0d3974afe127351b31bb8b2dcc7de57180
Grisha Pochuev <grisha854854@gmail.com>
2026-08-06 01:56:10 +0300

    Update axiom audit after cut-index refactor

--- Allender/AxiomAudit.lean
+++ Allender/AxiomAudit.lean
@@ -11,5 +11,5 @@ compilation.  No theorem below is allowed to depend on `sorryAx`.
 #print axioms Allender.Rel.comp_assoc
 #print axioms Allender.Rel.composeList_append
 #print axioms Allender.Rel.Functional.comp
-#print axioms Allender.LayeredDigraph.blockIndex_succ_of_not_mem
+#print axioms Allender.cutCountBelow_succ_of_not_mem
 #print axioms Allender.LayeredDigraph.edge_same_block_of_source_survives
