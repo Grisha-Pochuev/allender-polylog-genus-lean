bb9e8eb885f1071abf312569fd8e5f88aa76d971
Grisha Pochuev <grisha854854@gmail.com>
2026-08-06 04:56:58 +0300

    Import separator arithmetic modules

--- Allender.lean
+++ Allender.lean
@@ -1,5 +1,7 @@
 import Allender.FiniteState
 import Allender.Relation
 import Allender.LayeredGraph
+import Allender.Halving
+import Allender.WeightedMedian
 import Allender.Interfaces.Complexity
 import Allender.Interfaces.Topology
